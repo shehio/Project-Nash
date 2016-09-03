eb5dff1de5c9325c157e97ff1073b0453d5ccae2
shehio <shehabyasser@gmail.com>
2016-09-03 11:11:54 -0400

    Added roughly tested mixed nash.

--- index.js
+++ index.js
@@ -33,7 +33,32 @@ function check_neighbors(matrix, row, col) {
         && current.lth(matrix[row][horizontal]));
 }
 
+function find_mixed(matrix) {
+    var mixed = new Array();
+    var r1c1 = matrix[0][0];
+    var r1c2 = matrix[0][1];
+    var r2c1 = matrix[1][0];
+    var r2c2 = matrix[1][1];
+    /**
+     *  p: probability that player one plays his/her first choice.
+     */
+    var p = (r2c2.y - r2c1.y) / (r1c1.y + r2c2.y - r2c1.y - r1c2.y);
+    var p1_average_payoff = r1c1.y * p + (1 - p) * r2c1.y; // or r1c2.y * p + (1 - p)  *r2c2.y
+
+    /**
+     *  q: probability that player two plays his/her first choice.
+     */
+    var q = (r2c2.x - r1c2.x) / (r1c1.x + r2c2.x - r2c1.x - r1c2.x);
+    var p2_average_payoff = r1c1.x * q + (1 - q) * r1c2.x; // or r2c1.x * p + (1 - p)  *r2c2.x
+
+    mixed.push({ p: p, average_payoff: p1_average_payoff });
+    mixed.push({ p: q, average_payoff: p2_average_payoff });
+
+    return mixed;
+}
+
 module.exports = {
-    check_neighbors: check_neighbors, 
-    find_pure: find_pure
+    check_neighbors: check_neighbors,
+    find_pure: find_pure,
+    find_mixed: find_mixed
 }
--- test/index.js
+++ test/index.js
@@ -5,6 +5,9 @@ const classes = require('./../classes.js')
 const Payoff = classes.Payoff;
 const Pair = classes.Pair;
 const nash = require('./../index.js');
+/**
+ *  @todo: seperate every test into a seperate file.
+ */
 
 var prisoners_dilemma = function () {
     var matrix = new Array();
@@ -20,6 +23,15 @@ var matching_pennies = function () {
     var arr = [new Payoff(1, -1), new Payoff(-1, 1)];
     matrix.push(arr);
     arr = [new Payoff(-1, 1), new Payoff(1, -1)];
+    matrix.push(arr);
+    return matrix;
+}
+
+var example = function() {
+    var matrix = new Array();
+    var arr = [new Payoff(3, -3), new Payoff(-2, 2)];
+    matrix.push(arr);
+    arr = [new Payoff(-1, 1), new Payoff(0, 0)];
     matrix.push(arr);
     return matrix;
 }
@@ -68,6 +80,81 @@ describe('matching pennies: ', () => {
         expect(pure).to.not.be.null;
         expect(pure).to.be.a('Array');
         expect(pure).to.be.empty;
+        done();
+    });
+
+    it('has mixed nash equilibrium', done => {
+        var expected = {p: 0.5, average_payoff: 0};
+        var mixed = nash.find_mixed(matrix);
+        expect(mixed).to.not.be.null;
+        expect(mixed).to.not.be.undefined;
+        expect(mixed).to.be.a('Array');
+        expect(mixed).to.not.be.empty;
+        expect(mixed).to.have.lengthOf(2);
+
+        var p1 = mixed.shift();
+        expect(p1).to.not.be.null;
+        expect(p1).to.not.be.undefined;
+        expect(p1).to.be.eql(expected);
+
+        var p2 = mixed.shift();
+        expect(p2).to.not.be.null;
+        expect(p2).to.not.be.undefined;
+        expect(p2).to.be.eql(expected);
+
+        done();
+    });
+});
+
+
+
+
+describe('random example: ', () => {
+    before(() => { matrix = example(); });
+    it('tests check_neighbors', (done) => {
+        const rows = 2;
+        const cols = 2;
+        var cn = (i, j) => nash.check_neighbors(matrix, i, j);
+        for (var i = 0; i < rows; i++) {
+            for (var j = 0; j < cols; ++j) {
+                assert.equal(cn(i, j), false);
+            }
+        }
+        done();
+    });
+
+    it('doesnt have a pure nash equilibrium', done => {
+        var pure = nash.find_pure(matrix);
+        expect(pure).to.not.be.undefined;
+        expect(pure).to.not.be.null;
+        expect(pure).to.be.a('Array');
+        expect(pure).to.be.empty;
+        done();
+    });
+
+    it('has mixed nash equilibrium', done => {
+        /**
+         *  @todo: change the extremely error prone decimals, to nearly equal.
+         */
+        var p1_expected = {p: 0.16666666666666666, average_payoff: 0.33333333333333337};
+        var p2_expected = {p: 0.3333333333333333, average_payoff: -0.3333333333333335};
+        var mixed = nash.find_mixed(matrix);
+        expect(mixed).to.not.be.null;
+        expect(mixed).to.not.be.undefined;
+        expect(mixed).to.be.a('Array');
+        expect(mixed).to.not.be.empty;
+        expect(mixed).to.have.lengthOf(2);
+
+        var p1 = mixed.shift();
+        expect(p1).to.not.be.null;
+        expect(p1).to.not.be.undefined;
+        expect(p1).to.be.eql(p1_expected);
+
+        var p2 = mixed.shift();
+        expect(p2).to.not.be.null;
+        expect(p2).to.not.be.undefined;
+        expect(p2).to.be.eql(p2_expected);
+
         done();
     });
 });
