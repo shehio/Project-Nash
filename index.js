c3edf933c0be69e38e1a9261298633de8749632b
shehio <shehabyasser@gmail.com>
2016-09-03 07:22:05 -0400

    Added tests for prisoners dilemma, and matching pennies.

--- index.js
+++ index.js
@@ -4,37 +4,27 @@ const Pair = classes.Pair;
 const rows = 2;
 const cols = 2;
 
-var matrix = new Array();
-var pure = new Array();
-
 /**
  *  Strive for no side effects.
  */
 
-function prepare_data() {
-    var arr = [new Payoff(-1, -1), new Payoff(-10, 0)];
-    matrix.push(arr);
-    arr = [new Payoff(0, -10), new Payoff(-5, -5)];
-    matrix.push(arr);
-}
-
-
-function find_pure() {
+function find_pure(matrix) {
+    var pure = new Array();
     for (var i = 0; i < rows; ++i) {
         for (var j = 0; j < cols; ++j) {
-            if (check_neighbors(i, j)) {
+            if (check_neighbors(matrix, i, j)) {
                 pure.push(new Pair(i, j));
-                console.log(pure);
             }
         }
     }
+    return pure;
 }
 /**
  *  As always, player one is represented by x in (x,y) coordinates, 
  *  and his/her choices is represented vertically.
  *  The exact opposite for player two.
  */
-function check_neighbors(row, col) {
+function check_neighbors(matrix, row, col) {
     var bool = true;
     var vertical = 1 - row;
     var horizontal = 1 - col;
@@ -43,5 +33,7 @@ function check_neighbors(row, col) {
         && current.lth(matrix[row][horizontal]));
 }
 
-prepare_data();
-find_pure();
+module.exports = {
+    check_neighbors: check_neighbors, 
+    find_pure: find_pure
+}
