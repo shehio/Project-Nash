33e18ccec2b40dbaaf6e039dbe976ee39539f3b7
shehio <shehabyasser@gmail.com>
2016-09-10 12:34:48 -0400

    Seperated coin, and pi, examples in montecarlo.

--- montecarlo/index.js
+++ montecarlo/index.js
@@ -22,63 +22,35 @@ function run(sample_number, probability_distribution, estimator_function) {
     return miu;
 }
 
-/**
- *  radius example
- */
 function random_point(dimension) {
-    
+
     if (dimension === null || dimension === undefined) {
         throw "dimension is null, or undefined";
     }
     return new Point(PD.rnorm(dimension, mean, sd));
 }
 
-function d2 (){
-    // let dimension = 2;
-    // return random_point(dimensions);
+/**
+ * @todo: fix discrepancies between d1 && d2 
+ */
+function d2() {
     let range = 2;
     let start = 0;
     return new Point(Math.random() * range - start, Math.random() * range - start);
 }
 
-function d1 (){
+function d1() {
     let dimension = 1;
     return random_point(dimension);
 }
 
-function check_inside(point) {
-    if (point === undefined || point === null) {
-        throw "point is null, or undefined";
-    }
-    var radius = 1;
-    var center = new Point(1, 1);
-    xc = center.x;
-    yc = center.y;
-    var x = point.x;
-    var y = point.y;
-    if ((x - xc) * (x - xc) + (y - yc) * (y - yc) < radius * radius) {
-        return 1;
-    }
-    return 0;
-}
-
-function calculate_radius(){
-    let sample_number = 100000;
-    let generator = d2;
-    let estimator_function = check_inside;
-    let area_of_square = 2 * 2;
-    return run(sample_number, generator, estimator_function) * area_of_square;
-}
-
-
-
 
 module.exports = {
     d1: d1,
-    d2: d2, 
-    run: run
+    d2: d2,
+    run: run, 
+    /**
+     *  not sure if it's a good practice.
+     */
+    Point: Point
 }
-
-
-
-console.log(calculate_radius());
---
+++ montecarlo/pi.js
@@ -0,0 +1,27 @@
+const mc = require('./index.js')
+
+function check_inside(point) {
+    if (point === undefined || point === null) {
+        throw "point is null, or undefined";
+    }
+    var radius = 1;
+    var center = new mc.Point(1, 1);
+    xc = center.x;
+    yc = center.y;
+    var x = point.x;
+    var y = point.y;
+    if ((x - xc) * (x - xc) + (y - yc) * (y - yc) < radius * radius) {
+        return 1;
+    }
+    return 0;
+}
+
+function calculate_radius(){
+    let sample_number = 100000;
+    let generator = mc.d2;
+    let estimator_function = check_inside;
+    let area_of_square = 2 * 2;
+    return mc.run(sample_number, generator, estimator_function) * area_of_square;
+}
+
+console.log(calculate_radius());
