85a1a78300abd457e93c0c556bb80fe889cf45fc
shehio <shehabyasser@gmail.com>
2016-10-02 08:27:58 -0400

    Added a default task for gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,42 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const jshint = require('gulp-jshint');
 
-gulp.task('default', ['jshint', 'test'], function () { });
+/**
+ *  all directories for watching, and code quality.
+ */
+const sources = [
+    './*.js',
+    './test/*.js',
+    './finance/**/*.js',
+    './lemke-howson/**/*.js',
+    './minmax/**/*.js',
+    './montecarlo/**/*.js'
+];
 
-gulp.task('test', () => {
-    let sources = [
-        './test/*.js',
-        './finance//test/*.js',
-        './lemke-howson/test/*.js',
-        './minmax/test/*.js',
-        './montecarlo/test/*.js'
-    ]
-    return gulp.src(sources, { read: false })
+const tests = [
+    './test/*.js',
+    './finance/test/*.js',
+    './lemke-howson/test/*.js',
+    './minmax/test/*.js',
+    './montecarlo/test/*.js'
+];
+gulp.task('default', ['jshint', 'test'], () => {
+    gulp.watch(sources, () => {
+        gulp.run('jshint');
+        gulp.run('test');
+    })
+});
+
+gulp.task('jshint', () => {
+    return gulp.src(sources)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', ['jshint'], () => {
+    return gulp.src(tests, { read: false })
         .pipe(mocha({
             reporter: 'spec',
             globals: {}
         }));
-});
-
-gulp.task('jshint', () => {
-    return gulp.src('./*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
 });
