975862c9854c86afebb9c409a8d91a9e444f6509
shehio <shehabyasser@gmail.com>
2017-04-03 08:10:26 -0700

    Yet more refactoring...

--- finance/models.js
+++ finance/models.js
@@ -4,7 +4,8 @@
  * Δt = 1 (1 day)
  * φ = normally distributed random number
 */
-let geometric_brownian = (estimators, time, random) => {
+let geometric_brownian = (estimators, time, random) => 
+{
     let miu = estimators.mean;
     let sigma = estimators.std;
 
@@ -12,10 +13,9 @@ let geometric_brownian = (estimators, time, random) => {
     let shock = sigma * random * time;
     console.log(`miu = ${miu}, sigma = ${sigma}, drift = ${drift}, shock = ${shock}`);
     let result = drift + shock;
-    // console.log(result);
     return result;
-}
+};
 
 module.exports = {
     geometric_brownian: geometric_brownian
-}
+};
--- finance/stock.js
+++ finance/stock.js
@@ -14,12 +14,11 @@ const to = 'to';
 const l1 = 'l1';
 const mean = 'mean';
 const std = 'std';
-/**
- *  @todo: add some external file for literals.
- */
 
+// @todo: add some external file for literals.
 
-var get_historical = (symbol, dates, period) => {
+var get_historical = (symbol, dates, period) => 
+{
   // only because of the lodash isPlainObject in yahoo-finance!
   let object = {
     'symbol': symbol,
@@ -28,77 +27,81 @@ var get_historical = (symbol, dates, period) => {
     'period': period
   };
   return yahoo.historical(object);
-}
+};
 
-var get_todays = (symbol, fields) => {
+var get_todays = (symbol, fields) => 
+{
   // only because of the lodash isPlainObject in yahoo-finance!
-  let object = {
+  let object = 
+  {
     'symbol': symbol,
     'fields': fields
   };
   return yahoo.snapshot(object);
-}
+};
 
-var preprocess = (quotes) => {
+var preprocess = (quotes) => 
+{
   quotes = quotes.map((quote) => quote.adjClose);
-  quotes = quotes.map((quote, index) => {
-    if (index == 0) {
+  quotes = quotes.map((quote, index) => 
+  {
+    if (index === 0) 
+    {
       return 0;
     }
+
     // ΔS for 1 day.
     // I have looked around if map provides any spatial object reference (previous/next), couldn't find any.
     // console.log( `quote = ${quote}, quotes[index - 1] = ${quotes[index - 1]}, delta_s = ${(quote - quotes[index - 1]) / quote}`);
     return (quote - quotes[index - 1]) / quote;
   });
+
   quotes = quotes.slice(one, quotes.length);
   return quotes;
-}
+};
 
-var calculate_estimators = (array) => {
-  let ret = new Object();
+var calculate_estimators = (array) => 
+{
+  let ret = {};
   ret[mean] = math.mean(array);
   ret[std] = math.std(array);
   return ret;
-}
+};
 
-var calculate_price = () => {
+var calculate_price = () => 
+{
   let sample_number = thousand;
   let generator = Math.random;
   let estimator_function = intermediate;
   return mc.run(sample_number, generator, estimator_function);
-}
+};
 
-var intermediate = (random) => { 
-  /** 
-   * @todo: these should change, and bind fields, to the whole Object to be, instead of useless function calls.
-   */
+var intermediate = (random) => 
+{ 
+  // @todo: these should change, and bind fields, to the whole Object to be, instead of useless function calls.
   let estimators = estimate();
-  // console.log(estimators);
-  // console.log(price());
   let delta = delta_time();
-  // console.log(models.geometric_brownian(estimators, delta, random) * price());
   return models.geometric_brownian(estimators, delta, random) * price();
-}
+};
 
-var experiment = () => {
+var experiment = () => 
+{
   let dates = new Map();
   dates[to] = moment().format(format);
   dates[from] = moment().subtract(one, year).calendar(null, { sameElse: format });
   let fields = [l1];
   get_todays(symbol, fields).
-    then((snapshot) => { module.original_price = snapshot.lastTradePriceOnly }).
+    then((snapshot) => module.original_price = snapshot.lastTradePriceOnly).
     then(() => get_historical(symbol, dates, period)).
     then((quotes) => preprocess(quotes)).
     then((data) => calculate_estimators(data)).
-    then((estimators) => { module.estimated = estimators }).
+    then((estimators) => module.estimated = estimators).
     then((estimators) => calculate_price()).
-    then((data => { console.log(data) })).
+    then((data => console.log(data))).
     catch((error) => console.log(error));
-}
+};
 
-/**
- *  just a workaround, in order to keep the simplistic montecarlo implementation.
- */
+// A workaround, in order to keep the simplistic montecarlo implementation.
 var module = {
   estimated: 0, // to be set.
   original_price: 0, // to be set.
@@ -110,8 +113,5 @@ var module = {
 let estimate = module.estimate.bind(module);
 let price = module.price.bind(module);
 let delta_time = module.delta_time.bind(module);
-
-// console.log(module.delta_time())
-// console.log(calculate_price());
 
 experiment();
