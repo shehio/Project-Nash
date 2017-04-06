2ef2db02859822495aec94254cdfa790c3b63b91
shehio <shehabyasser@gmail.com>
2017-04-05 23:07:13 -0700

    Added white, tests await.

--- econometrics/index.js
+++ econometrics/index.js
@@ -1,4 +1,5 @@
-var stats = require("simple-statistics");
+const stats = require('simple-statistics');
+const regression = require('regression');
 
 // let's begin with the simplist hypothesis, Gauss–Markov assumption are satistfied,
 // i.e. OLS is BLUE, we will have to perform an OLS just before a t-test
@@ -113,52 +114,191 @@ let gdp =
 3007.1
 ];
 
-let data = new Array();
-let thousand = 1000;
-let fifty = 50;
 
-// reformat the data
-for(let i = fifty - 1; i > 0 - 1; i--)
+// t-test: significance of linear regression, 
+// searched a lot for something ready, didn't find any for regression.
+// simple-statistics has a traditional t-test (one way and two ways).
+var regression_significance = function (x, y, mu = 0)
 {
-    data.push(new Array(gdp[i] / thousand, life_expecatncy[i]));
+    let data = [];
+    let average = 0;
+    let std_error = 0;
+    let numerator = 0;
+    let denum = 0;
+    let residuals = [];
+    let estimated = [];
+    let n = x.length;
+    let t = 0;
+
+    // reformat the data
+    for(let i = 0; i < n; i++)
+    {
+        data.push(new Array(x[i], y[i]));
+    }
+
+    // regression
+    let result = stats.linearRegression(data);
+    let m = result.m;
+    let b = result.b;
+
+    // calculate residuals
+    for(let i = 0; i < n; i++) 
+    {
+        residuals[i] = ( x[i] * m + b - y[i]);
+        average += x[i];
+    }
+
+    average /= n;
+
+    for(let i = 0; i < n; i++) 
+    {
+        numerator += residuals[i] * residuals[i];
+        denum += (x[i] - average) * (x[i] - average);
+    }
+
+    numerator /= (n - 2);
+    std_error = Math.sqrt(numerator/denum);
+    t = (m - mu) / std_error;
+
+    return {
+        b: b, 
+        m: m, 
+        residuals: residuals,
+        std_error: std_error, 
+        t: t
+    };
 }
 
-// regression
-let result = stats.linearRegression(data);
-let m = result.m;
-let b = result.b;
-console.log(result);
-
-// calculate residuals
-let average = 0;
-let std_error = 0;
-let numerator = 0;
-let denum = 0;
-let residuals = new Array();
-let estimated = new Array();
-
-for(let i = 0; i < fifty; i++) 
-{
-    residuals[i] = (gdp[i] / thousand * m + b - life_expecatncy[i]);
-    average += gdp[i] / thousand;
-}
-
-average /= fifty;
-
-for(let i = 0; i < fifty; i++) 
-{
-    numerator += residuals[i] * residuals[i];
-    denum += (gdp[i] / thousand - average) * (gdp[i] / thousand - average);
-}
-
-numerator /= (fifty - 2);
-std_error = Math.sqrt(numerator/denum);
 
 // According to t-table for a two tail test with confidence level 99.9%
 // t should be around 3.5, we have it at 22 which means we are beyond
 // any doubt
-let t = (m - 0) / std_error;
-console.log(`standard error = ${std_error}`);
-console.log(`t = ${t}`);
+let thousand = 1000;
+let ret = regression_significance(gdp.map((x) => x / thousand), life_expecatncy, 0);
+console.log(`t = ${ret.t}`);
 console.log("We reject the null hypothesis, and accept the alternative hypothesis.");
 console.log("Link to t-table: http://www.sjsu.edu/faculty/gerstman/StatPrimer/t-table.pdf")
+
+// now we have to figure out whether OLS is really BLUE or not, i.e.whether
+// it satistfies the Gauss–Markov assumptions.
+
+// white's test for heteroskedasticity:
+
+// û = αo + α1 ŷ + α2 ŷ ^ 2
+// regress û on ŷ and ŷ squared.
+// this is a multiregression problem which can't be dealt with using linear regression.
+// it can also just be considered polynomial regression of degree two (makes things easier)
+
+// û (pronunced u circumflex) is merely the difference between the actual 
+// value and the estimator (regressed value) using the aforedescribed OLS.
+// we've already got it: residuals array.
+
+// change format of the data to be compliant with regression.js
+var white = function (x, m, b, residuals)
+{
+    let awry = [];
+    let y_hat = [];
+    let y_bar = 0;
+    let data = [];
+    let n = residuals.length;
+
+    for(let i = 0; i < n; i++)
+    {   
+        y_hat.push(m * x[i] + b);
+        y_bar += y_hat[i];
+
+        awry.push(y_hat[i]);
+        awry.push(residuals[i]);
+
+        data.push(awry);
+        awry = [];
+    }
+
+    result = regression('polynomial', data, 2);
+
+    y_bar /= n;
+    let coeff = result.equation;
+    console.log(coeff);
+
+
+    // *******************
+    // Create an ANOVA method starting here!
+
+    // now we test the significance of the regression, 
+    // Ho: αo = α1 = α2 = 0, 
+    // Ha: αj ≠ 0, ∃ j
+    // we are going to use the F-test (ANOVA)
+    // Fo = MSr/MSe, MSr: regression mean square, while MSe: error mean square.
+
+    // The total variability of the observed data, i.e. the total sum of squares can be accounted for 
+    // using of the variability explained by the model , and the portion unexplained by the model
+    // SSt = SSr + SSe
+
+    // The above equation is also referred to as the analysis of variance identity and can be expanded as follows:  
+    // Σ(yi - ȳ) ^ 2 = Σ(ŷi - ȳ) ^ 2 + Σ(yi - ŷ) ^ 2
+    // y is just a symbol for a standard eqn, what we are looking for here is u.
+
+    // MSr = SSr /  d.f, where d.f = n (data points) + regressors - 1
+    // MSe = SSe / d.f, where d.f = n (data points) + regressors - 1
+
+    // To cut a long story short: calculate the F statistic, and then there is a look-up table.
+
+
+    // we've got ui ~ observation, we lack ui circumflex, and u bar
+    // ui is the residuals, ui hat is the residuals of the estimators of the residuals.
+    // u bar is just the average
+
+    let u_hat = [];
+    let alpha_node = coeff[0];
+    let alpha_one = coeff[1];
+    let alpha_two = coeff[2];
+    let regressor = 0;
+    let u_bar = 0;
+
+    for(let i = 0; i < n; i++)
+    {
+        regressor = y_hat[i];
+        u_hat.push(alpha_node + alpha_one * regressor + alpha_two * regressor * regressor);
+        u_bar += residuals[i];
+    }
+
+    u_bar /= n;
+
+    // change notattion to make it easy again:
+    x = y_hat;
+    let x_bar = y_bar;
+    y = y_hat;
+    y_hat = u_hat;
+    y_bar = u_bar;
+    
+    let SSR = 0;
+    let SSE  = 0;
+    let SST = 0;
+    let df = n - 1;
+
+    for(let i = 0; i < n; i++)
+    {
+        SSR += (y_hat[i] - y_bar) * (y_hat[i] - y_bar);
+        SSE += (y[i] - y_hat[i]) * (y[i] - y_hat[i]);
+        SST += (y[i] - y_bar);
+    }
+
+    // n is the number of observations, p is the number of regression parameters
+    let p = 2;
+    // or DFR 
+    let DFM = p - 1 
+    // degrees of freedom of the error 
+    let DFE = n - p 
+    // total degrees of freedom
+    let DFT = n - 1
+
+    // or MSM 
+    let MSR = SSR / DFM;
+    let MSE = SSE / DFE;
+    let f = MSR/MSE;
+
+    return f;
+}
+
+console.log(white(gdp.map((x) => x / thousand), ret.m, ret.b, ret.residuals));
+
