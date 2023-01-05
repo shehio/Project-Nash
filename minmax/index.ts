2f39a721e4be230ac291e5765ea7751b78c76299
shehio <shehabyasser@gmail.com>
2023-01-04 18:53:47 -0800

    Modified minmax

--- minmax/index.ts
+++ minmax/index.ts
@@ -1,32 +1,46 @@
 // This code doesn't return the current move.
 // It returns where will eventually end up.
+
+// player == 0 is player 1
+// while player == 1, is player 2
 export function minmax(root, player) {
     let min = false;
     if (player == 0) {
         min = true;
     }
     if (root.is_leaf()) {
-        return root;
+        console.log('Leaf, returning, value:  ' + root.name)
+        console.log('Leafs payoff: ' + root.payoff)
+        if (min) {
+            root.value = root.payoff.x
+        } else {
+            root.value = root.payoff.y
+        }
+        
+        return root
     }
 
-    var ret = null;
-    var max = -Infinity;
+    var move = null;
+    var maximum = -Infinity;
+    var minimum = Infinity;
 
     for(let i = 0; i < root.children.length; i++) {
         let child  = root.children[i];
+        console.log(child.name)
         var result = minmax(child, 1 - player);
         if (min) {
-            if (result.value.x > max) {
-                max = result.value.x;
-                ret = result;
+            if (root.value < minimum) {
+                minimum = result.payoff.x;
+                move = result;
             }
         }
         else {
-            if (result.value.y > max) {
-                max = result.value.y;
-                ret = result;
+            if (root.value > maximum) {
+                maximum = result.payoff.y;
+                move = result;
             }
         }
     }
-    return ret;
+
+    return move;
 }
