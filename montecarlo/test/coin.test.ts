bb2f663621b238373714a104cba95295ea705285
shehio <shehabyasser@gmail.com>
2023-01-07 21:07:37 -0800

    Added another coin test

--- montecarlo/test/coin.test.ts
+++ montecarlo/test/coin.test.ts
@@ -9,19 +9,22 @@ const check_mean = (point) => {
     return false;
 }
 
-const coin_flip = () => {
-    let sample_number = 100000;
+const coin_flip = (sample_number: number = 100000) => {
     let generator = d1; 
     let estimator_function = check_mean;
     return run(sample_number, generator, estimator_function);
 }
 
-console.log(coin_flip());
-
 describe('Montecarlo coin flip: ', () => {
-    it('works', () => {
+    it('convergest with enough sample size', () => {
         const flip = coin_flip()
         expect(flip).toBeCloseTo(0.5, 2)
+
+    });
+
+    it('does not converge with smaller sample size', () => {
+        const flip = coin_flip(1)
+        expect(flip).not.toBeCloseTo(0.5, 2)
 
     });
 });
